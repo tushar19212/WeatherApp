e34085a0c1a1ec8e312e1623bed5b458a376f67a
Tushar Agarwal <55681053+tushar19212@users.noreply.github.com>
2020-08-04 03:31:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+    let temperatureDescription=document.querySelector('.temperature-description');
+    let temperatureDegree=document.querySelector('.temperature-degree');
+    let locationTimezone=document.querySelector('.location-timezone');
+    let Icon=document.querySelector('.icons');
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long=position.coords.longitude;
+            lat=position.coords.latitude;
+            const api = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${long}&days=1&key=e3ce543b242141de941a30386d48d39a`;
+            fetch(api).then(response => {
+                    return response.json();
+            }).then(data =>{
+                // console.log(data);
+                const {city_name} = data;
+                const {temp}=data.data[0];
+                const {description,icon}=data.data[0].weather;
+                //Set DOM Elements
+                temperatureDegree.textContent = temp;
+                temperatureDescription.textContent = description;
+                locationTimezone.textContent = city_name;
+                Icon.src="icons/"+icon+".png";
+
+            });
+        });
+    }
+    else{
+        alert("Doesn't Support");
+    }
+});
